81ea1c5c5cf8a8b2c66d880a2ea10bf4dd34dbf6
iamozansson <ozan.arikan@hotmail.com>
2026-02-24 20:18:34 +0200

    Finalize TicTacToe project

--- main.py
+++ main.py
@@ -20,3 +20,125 @@ def printGameBoard():
             print('', gameBoard[x][y], end=' |')
     print('\n+---+---+---+')
 printGameBoard()
+
+def modifyArray(num, turn):
+    num -= 1
+    if(num == 0):
+        gameBoard[0][0] = turn
+    elif(num == 1):
+        gameBoard[0][1] = turn
+    elif(num ==2):
+        gameBoard[0][2] = turn
+    elif(num ==3):
+        gameBoard[1][0] = turn
+    elif(num ==4):
+        gameBoard[1][1] = turn
+    elif(num ==5):
+        gameBoard[1][2] = turn
+    elif(num ==6):
+        gameBoard[2][0] = turn
+    elif(num ==7):
+        gameBoard[2][1] = turn
+    elif(num ==8):
+        gameBoard[2][2] = turn
+def checkForWinner(gameboard):
+    ### X axis
+    if(gameBoard[0][0] == 'X' and gameBoard[0][1] == 'X' and gameBoard[0][2] == 'X'):
+        print('X has won!')
+    
+    elif(gameBoard[0][0] == 'O' and gameBoard[0][1] == 'O' and gameBoard[0][2] == 'O'):
+        print('O has won!')
+        return 'O'
+    
+    elif(gameBoard[1][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[1][2] == 'X'):
+        print('X has won!')
+        return 'X'
+    
+    elif(gameBoard[1][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[1][2] == 'O'):
+        print('O has won!')
+        return 'O'
+    
+    elif(gameBoard[2][0] == 'X' and gameBoard[2][1] == 'X' and gameBoard[2][2] == 'X'):
+        print('X has won!')
+        return 'X'
+    
+    elif(gameBoard[2][0] == 'O' and gameBoard[2][1] == 'O' and gameBoard[2][2] == 'O'):
+        print('O has won!')
+        return 'O'
+    
+    ### Y axis
+    elif(gameBoard[0][0] == 'X' and gameBoard[1][0] == 'X' and gameBoard[2][0] == 'X'):
+        print('X has won!')
+        return 'X'
+    
+    elif(gameBoard[0][0] == 'O' and gameBoard[1][0] == 'O' and gameBoard[2][0] == 'O'):
+        print('O has won!')
+        return 'O'
+    
+    elif(gameBoard[0][1] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][1] == 'X'):
+        print('X has won!')
+        return 'X'
+
+    elif(gameBoard[0][1] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][1] == 'O'):
+        print('O has won!')
+        return 'O'
+    
+    elif(gameBoard[0][2] == 'X' and gameBoard[1][2] == 'X' and gameBoard[2][2] == 'X'):
+        print('X has won!')
+        return 'X'
+    
+    elif(gameBoard[0][2] == 'O' and gameBoard[1][2] == 'O' and gameBoard[2][2] == 'O'):
+        print('O has won!')
+        return 'O'
+
+    ### Cross wins
+    elif(gameBoard[0][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][2] == 'X'):
+        print('X has won!')
+        return 'X'
+    
+    elif(gameBoard[0][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][2] == 'O'):
+        print('O has won!')
+        return 'O'
+
+    elif(gameBoard[0][2] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][0] == 'X'):
+        print('X has won!')
+        return 'X'
+    
+    elif(gameBoard[0][2] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][0] == 'O'):
+        print('O has won!')
+        return 'O'
+    
+    else:
+        return 'N'
+
+leaveLoop = False
+turnCounter = 0
+
+while(leaveLoop == False):
+    ### It's the player turn
+    if(turnCounter % 2 == 1):
+        printGameBoard()
+        numberPicked = int(input('\n Please choose a number [1=9]: '))
+        if(1 <= numberPicked <= 9 and numberPicked in possibleNumbers):
+            modifyArray(numberPicked, 'X')
+            possibleNumbers.remove(numberPicked)
+            turnCounter += 1
+        else:
+            print('Invalid input, please pick another number')
+    ### It's computer's turn
+    else:
+        while(True):
+
+            cpuChoice = random.choice(possibleNumbers)
+            print('\nComputer choice: ', cpuChoice)
+
+            if(cpuChoice in possibleNumbers):
+                modifyArray(cpuChoice, 'O')
+                possibleNumbers.remove(cpuChoice)
+                turnCounter += 1
+                break
+    
+    winner = checkForWinner(gameBoard)
+    if(winner != "N"):
+        print("\nGame over! Thank you for playing :)")
+        break
